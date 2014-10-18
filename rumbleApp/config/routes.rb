decfc8efd8a2efe9802f3d90046a33f1bf98098a
Kim Pham <ms.kimpham@gmail.com>
2014-10-17 18:28:57 -0700

    added seed database

--- rumbleApp/config/routes.rb
+++ rumbleApp/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :categories, only: [:index, :show, :create] do 
+    resources :entities, only: [:index, :show, :create]
+    resources :scales, only: [:index, :show, :create]
+  end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- rumbleApp/db/seeds.rb
+++ rumbleApp/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  categories = Category.create([{name: 'Tech'}, {name: 'Cities'}])
+  entities = Entity.create([{name: 'Uber', category_id: categories[0].id}, {name: 'Lyft', category_id: categories[0].id}, 
+    {name: 'San Francisco', category_id: categories[1].id}, {name: 'New York', category_id: categories[1].id}])
+  scales = Scale.create([{name: 'Success', positive: 'Game Changer', negative: 'Cautionary Tale', category_id: categories[0].id}, 
+    {name: 'Usefulness', positive: 'Useful', negative: 'Useless', category_id: categories[0].id},
+    {name: 'Cost of Living', positive: 'Cheap', negative: 'Expen$ive', category_id: categories[1].id},
+    {name: 'Attractive People', positive: 'Beautiful', negative: 'Ugly', category_id: categories[1].id},
+    ])
+  join = EntitiesScale.create([{entity_id: entities[0].id, scale_id: scales[0].id, score: 5, total: 10},
+    {entity_id: entities[0].id, scale_id: scales[1].id, score: 6, total: 10},
+    {entity_id: entities[1].id, scale_id: scales[0].id, score: 4, total: 10},
+    {entity_id: entities[1].id, scale_id: scales[1].id, score: 3, total: 10},
+    {entity_id: entities[2].id, scale_id: scales[2].id, score: 7, total: 10},
+    {entity_id: entities[2].id, scale_id: scales[3].id, score: 8, total: 10},
+    {entity_id: entities[3].id, scale_id: scales[2].id, score: 9, total: 10},
+    {entity_id: entities[3].id, scale_id: scales[3].id, score: 10, total: 10},
+    ])

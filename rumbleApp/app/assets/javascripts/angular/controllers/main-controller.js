be8dacebe7ae09ca1f861613c9496f723f37e0fd
Daniel Olasky <daniel.olasky@gmail.com>
2014-10-19 13:03:17 -0700

    animations! graphs! buttons

--- rumbleApp/app/assets/javascripts/angular/controllers/main-controller.js
+++ rumbleApp/app/assets/javascripts/angular/controllers/main-controller.js
@@ -8,8 +8,13 @@ app.controller('MainController', ['$scope', 'Category', 'Scale', 'Entity', 'Enti
     });
     $scope.entities = Entity.query();
     // $scope.entityScore = EntityScore.query();
-    $scope.entityScore = EntityScore.query({category_id: 1, x_scale_id: Math.floor(Math.random()*6)+1, y_scale_id: Math.floor(Math.random()*6)+1});
-
+    $scope.new_circle_array = [];
+    
+    $scope.load = function(){
+      $scope.entityScore = EntityScore.query({category_id: $scope.x.category_id, x_scale_id: Math.floor(Math.random()*6)+1, y_scale_id: Math.floor(Math.random()*6)+1}, function(data){
+        $scope.graphScores();
+      });
+    };
 
     $scope.getScales = function(category){
       Scale.query({category_id: category.id}, function(data){
@@ -19,6 +24,9 @@ app.controller('MainController', ['$scope', 'Category', 'Scale', 'Entity', 'Enti
       });
       Entity.query({category_id: category.id}, function(data){
         $scope.entities = data;
+        $scope.entityScore = EntityScore.query({category_id: $scope.x.category_id, x_scale_id: Math.floor(Math.random()*6)+1, y_scale_id: Math.floor(Math.random()*6)+1}, function(data){
+        $scope.graphScores();
+      });
       });
     };
 
@@ -29,55 +37,54 @@ app.controller('MainController', ['$scope', 'Category', 'Scale', 'Entity', 'Enti
     };
 
     $scope.randomize = function(){
-      $scope.entityScore = EntityScore.query({category_id: 1, x_scale_id: Math.floor(Math.random()*6)+1, y_scale_id: Math.floor(Math.random()*6)+1});
-    }
+      $scope.entityScore = EntityScore.query({category_id: $scope.x.category_id, x_scale_id: Math.floor(Math.random()*6)+1, y_scale_id: Math.floor(Math.random()*6)+1}, function(){
+        $scope.load();
+      });
+    };
 
     $scope.graphScores = function(){
       $("#graph").empty();
       var paper = new Raphael(document.getElementById('graph'));
-      // var yAxis = paper.path("M 250 0 l 0 500");
-      // var xAxis = paper.path("M 0 250 l 500 0");
-      var circle_array = [];
+      var yAxis = paper.path("M 250 0 l 0 500");
+      var xAxis = paper.path("M 0 250 l 500 0");
+      $scope.circle_array = [];
+      $scope.label_array = [];
             // var maxscale = $scope.entityScoreRand;
-      $scope.entityScore = EntityScore.query({category_id: 1, x_scale_id: Math.floor(Math.random()*6)+1, y_scale_id: Math.floor(Math.random()*6)+1}, function() {
-        var xValues = [];
-        var yValues = [];
-        var totals = [];
-        var labels = [];
         colors = ["#FFFF33", "#FF6600", "#FF3366", "#CCFFCC"]
-        for (var i = 0; i < $scope.entityScore.length; i++) {
-          xValues.push($scope.entityScore[i].x.score);
-          yValues.push($scope.entityScore[i].y.score);
-          totals.push($scope.entityScore[i].x.total);
-            // var c = paper.circle(250+(($scope.entityScore[i].x.score)*25), (250-($scope.entityScore[i].y.score)*25), 20);
-            // c.attr({fill: '#'+Math.floor(Math.random()*16777215).toString(16), "fill-opacity": 0.5, "stroke-opacity":0.1});
-            // var l = paper.text(240+(($scope.entityScore[i].x.score)*25), (275-($scope.entityScore[i].y.score)*25), $scope.entityScore[i].name)
-            // console.log($scope.entityScore[i].x.score)
-            // console.log($scope.entityScore[0].x.scale_id)
-          }
-          console.log(xValues);
-          console.log(yValues);
-          console.log(totals);
-          paper.dotchart(0, 0, 500, 500, xValues, yValues, totals, {heat: true, max: 10, opacity: 0.5, axis:"0 0 1 1", axisxstep: 1, axisystep: 1, axisxtype: " ", axisytype: " "}).each(function(){
-            paper.tag(this.value).insertBefore(this);
-            this.marker.show();
-          });
-      });      
+      for (var i = 0; i < $scope.entityScore.length; i++) {
+          var c = paper.circle(250+(($scope.entityScore[i].x.score)*25), (250-($scope.entityScore[i].y.score)*25), 20);
+          $scope.circle_array.push(c);
+          c.attr({fill: '#'+Math.floor(Math.random()*16777215).toString(16), "fill-opacity": 0.5, "stroke-opacity":0.1});
+          var l = paper.text(240+(($scope.entityScore[i].x.score)*25), (275-($scope.entityScore[i].y.score)*25), $scope.entityScore[i].name)
+          $scope.label_array.push(l);
+        }
+      console.log($scope.label_array);
     };
 
-
+    $scope.reDrawScores = function(x_scale, y_scale, cat){
+        // var paper = new Raphael(document.getElementById('graph'));
+      $scope.entityScore = EntityScore.query({category_id: x_scale.category_id, x_scale_id: x_scale.id, y_scale_id: y_scale.id}, function(data){
+          console.log(data);
+        for (var i = 0; i < $scope.circle_array.length; i++) {
+         $scope.circle_array[i].animate({cx: 250+(data[i].x.score)*25, cy: 250-(data[i].y.score)*25}, 2000, "elastic");
+         $scope.label_array[i].animate({x: 240+(data[i].x.score)*25, y: 275-(data[i].y.score)*25}, 2000, "bounce");
+        }
+      });
+    };
     // set x-axis labels
     $scope.xScale = function(scale) {
-      Scale.query({category_id: scale.category_id, scale_id: scale.id}, function(scale) {
+      Scale.get({category_id: scale.category_id, scale_id: scale.id}, function(scale) {
         $scope.x = scale;
+        $scope.reDrawScores($scope.x, $scope.y, $scope.x.category_id);
         // Entity.query({});
       });
     };
 
     // set y-axis labels
     $scope.yScale = function(scale) {
-      Scale.query({category_id: scale.category_id, scale_id: scale.id},function(scale) {
+      Scale.get({category_id: scale.category_id, scale_id: scale.id},function(scale) {
         $scope.y = scale;
+        $scope.reDrawScores($scope.x, $scope.y, $scope.x.category_id);
       });    
     };
 
